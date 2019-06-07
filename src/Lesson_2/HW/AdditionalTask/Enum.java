328da3cc854170a0af5290d5ce43114b54f1a174
iourilitv <iourilitv1965@gmail.com>
2019-06-07 09:52:42 +0300

    Created HW packages and HW tasks classes with tasks content.

---
+++ src/Lesson_2/HW/AdditionalTask/Enum.java
@@ -0,0 +1,20 @@
+package Lesson_2.HW.AdditionalTask;
+
+/**
+ * Java Core. Продвинутый уровень.
+ * Вебинар 03 июня 2019 MSK (UTC+3).
+ * Урок 2. eNum. Вложенные классы. Исключения.
+ * Home Work. Additional Task.
+ * @author Yuriy Litvinenko.
+ * Требуется реализовать enum DayOfWeek, который будет представлять дни недели.
+ * С его помощью необходимо решить задачу определения кол-ва рабочих часов
+ *  до конца недели по заданному текущему дню.
+ *  Возвращает кол-во оставшихся рабочих часов до конца недели по заданному текущему дню.
+ *  Считается, что текущий день ещё не начался, и рабочие часы за него должны учитываться.
+ * public class DayOfWeekMain {
+ *  public static void main(final String[] args) {
+ *   System.out.println(getWorkingHours(DayOfWeek.MONDAY));
+ *   }
+ */
+public class Enum {
+}
