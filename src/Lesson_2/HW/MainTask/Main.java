e03a2fd0c2e81b9890dd32aabe6e6597cd7163fd
iourilitv <iourilitv1965@gmail.com>
2019-06-07 13:57:23 +0300

    Added commits

--- src/Lesson_2/HW/MainTask/Main.java
+++ src/Lesson_2/HW/MainTask/Main.java
@@ -4,10 +4,10 @@ public class Main {
     public static void main(String[] args) {
         int maxArrayLength = 4;
         String[][] stringsArray = {
-                {"11", "12", "1a3", "14"},
+                {"11", "12", "13", "14"},
                 {"21", "22", "23", "24"},
                 {"31", "32", "3/3", "34"},
-                {"41", "42", "43", "44"}
+                {"41", "42", "43", "44", "55"}
         };
 
         int sum = MyExceptions.getArrayElementsSum(stringsArray, maxArrayLength);
--- src/Lesson_2/HW/MainTask/MyExceptions.java
+++ src/Lesson_2/HW/MainTask/MyExceptions.java
@@ -17,7 +17,12 @@ package Lesson_2.HW.MainTask;
  * исключения MySizeArrayException и MyArrayDataException и вывести результат расчета.
 */
 class MyExceptions {
-
+    /**
+     * Метод отлавливания исключений при вычислении суммы элементов массива
+     * @param stringsArray - массив чисел ввиде строк
+     * @param maxArrayLength - максимальная длина массива (здесь квадрат).
+     * @return сумму элементов массива, если нет исключений, иначе -1.
+     */
     public static int getArrayElementsSum(String[][] stringsArray, int maxArrayLength){
         int arrayElementsSum = -1;
         try{
@@ -37,17 +42,31 @@ class MyExceptions {
         return arrayElementsSum;
     }
 
+    /**
+     * Метод отлавливания исключения превышения длины массива
+     * @param stringsArray - массив чисел ввиде строк
+     * @param maxArrayLength - максимальная длина массива (здесь квадрат).
+     * @throws MyArraySizeException - исключение превышения длины массива по двум размерам
+     */
     public static void testArraySizeException(
                             String[][] stringsArray, int maxArrayLength
                                               ) throws MyArraySizeException{
 
-        if (stringsArray.length > maxArrayLength) throw new MyArraySizeException
-                (
-                "Длина массива превышает " + maxArrayLength + "!"
-                );
+        for (int i = 0; i < stringsArray.length; i++) {
+            if (stringsArray.length > maxArrayLength || stringsArray[i].length > maxArrayLength) throw new MyArraySizeException
+                    (
+                            "Длина массива превышает " + maxArrayLength + "!"
+                    );
+        }
         System.out.println("Array length is correct!");
     }
 
+    /**
+     * Метод отлавливания исключения если элемент массива не является числом в виде строки
+     * @param stringsArray - массив чисел ввиде строк
+     * @return сумму элементов массива, если нет исключений
+     * @throws MyArrayDataException - исключение если в элементе массива есть нечисла в виде строки
+     */
     public static int testArrayDataException(String[][] stringsArray) throws MyArrayDataException{
         int arrayElementsSum = 0;
         int num;
@@ -58,7 +77,7 @@ class MyExceptions {
                     num = Integer.parseInt(stringsArray[i][j]);
                     arrayElementsSum += num;
                 }
-                catch (NumberFormatException e){
+                catch (NumberFormatException e){//перехватываем стандартное исключение неверного формата числа
                     throw new MyArrayDataException
                             (
                              "Элемент массива stringArray[" + i + "][" + j + "]:\""
